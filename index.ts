d2654e875b8650fc41888739857c811ee1e11ff2
Omowunmi <sogunledolapo@gmail.com>
2025-01-27 22:02:53 +0100

    fixes

--- index.ts
+++ index.ts
@@ -33,23 +33,3 @@ class Clutta {
 }
 
 export default Clutta;
-
-const clutta = new Clutta({
-  apiKey: 'AF4Rzz4Ww0KTt7nI.f23f5a44-fa59-4a77-b5cf-951a18fb4ccb',
-  version: 'v0.0.10',
-});
-
-
-clutta.sendPulse(
-  {
-    signatureId: '1c5cfbed-25d7-463b-b430-8c59d729083f',
-    correlationId: 'f3c8b24a-98a2-4757-bb85-6bc1d768f7e2',
-    chainId: '3d6b57ec-43e4-43cd-88cc-e686288e0a05',
-    userId: 'wunmi@gmail.com',
-    sourceId: 'profile',
-    status: 1,
-    statusDescription: 'Success',
-  },
- 
-);
-
